e272ad89ea254ce27c904a7fb073a03f854b710c
Scott M Miller <smmiller.work@gmail.com>
2015-05-05 16:17:16 -0600

    step 5 completed

--- js/MainController.js
+++ js/MainController.js
@@ -1,7 +1,11 @@
 var app = angular.module('userProfiles');
 
-app.controller('MainController', function($scope) {
+app.controller('MainController', function($scope, mainService) {
 
-	
+	$scope.getUsers = function() {
+		$scope.users = mainService.getUsers();
+	};
+
+	$scope.getUsers();
 
 });
